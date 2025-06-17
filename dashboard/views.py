1b3edd3dd6fd4f6c8df815a0e83e46286355bb84
Lucas Oliveira <lucasio2008@gmail.com>
2025-06-17 19:16:24 -0300

    feat: Adiciona repository do workspace para a views do dashboard.

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.views import View
 
+from workspace.service import WorkSpaceRepository
+
 
 class DashboardView(View):
     template_name = 'dashboard/dashboard.html'
 
     def get(self, request):
-        return render(request, self.template_name)
+        work_space = WorkSpaceRepository.get_all_work_space(request.user)
+        context = {
+            'work_space': work_space
+        }
+        return render(request, self.template_name, context)
