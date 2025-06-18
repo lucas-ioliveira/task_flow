c6ed7591f7d569e088c79f0f66bbae5ca1773589
Lucas Oliveira <lucasio2008@gmail.com>
2025-06-18 20:24:41 -0300

    feat: Adiciona dados numéricos das tarefas para o template.

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 
 from workspace.service import WorkSpaceRepository
+from tasks.repository import TaskRepository
 
 
 class DashboardView(View):
@@ -9,7 +10,17 @@ class DashboardView(View):
 
     def get(self, request):
         work_space = WorkSpaceRepository.get_all_work_space(request.user)
+
+        done = TaskRepository.get_tasks_by_status(None, request.user, 'Done')
+        todo = TaskRepository.get_tasks_by_status(None, request.user, 'To do')
+        in_progress = TaskRepository.get_tasks_by_status(None, request.user, 'In progress')
+        paused = TaskRepository.get_tasks_by_status(None, request.user, 'Paused')
+
         context = {
-            'work_space': work_space
+            'work_space': work_space,
+            'done': done,
+            'todo': todo,
+            'in_progress': in_progress,
+            'paused': paused
         }
         return render(request, self.template_name, context)
